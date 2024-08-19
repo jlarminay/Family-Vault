c546022a09451708802110b30c5fa80733f9f86e
Josh <josh@ursacreative.ca>
2024-08-18 20:36:55 -0700

    Update app

--- .photo-app/functions/image_utils.py
+++ .photo-app/functions/image_utils.py
@@ -1,23 +1,26 @@
-from PIL import Image
 import os
+from PIL import Image
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, NEXT_IMAGE_PATH
 
-def resize_and_pad_image(image_path, output_path, screen_width, screen_height):
+def resize_and_pad_image(image_path, output_path):
+    """Resize and pad image to fit screen dimensions"""
     with Image.open(image_path) as img:
         img_ratio = img.width / img.height
-        screen_ratio = screen_width / screen_height
+        screen_ratio = SCREEN_WIDTH / SCREEN_HEIGHT
 
         if img_ratio > screen_ratio:
-            new_width = screen_width
+            new_width = SCREEN_WIDTH
             new_height = round(new_width / img_ratio)
         else:
-            new_height = screen_height
+            new_height = SCREEN_HEIGHT
             new_width = round(new_height * img_ratio)
 
         img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
-        new_img = Image.new("RGB", (screen_width, screen_height))
-        paste_position = ((screen_width - new_width) // 2, (screen_height - new_height) // 2)
+        new_img = Image.new("RGB", (SCREEN_WIDTH, SCREEN_HEIGHT))
+        paste_position = ((SCREEN_WIDTH - new_width) // 2, (SCREEN_HEIGHT - new_height) // 2)
         new_img.paste(img, paste_position)
         new_img.save(output_path)
 
 def display_image(image_path):
+    """Display image on the screen"""
     os.system(f'fbi -T 1 -d /dev/fb0 -noverbose -a {image_path} > /dev/null 2>&1')
--- .photo-app/init.sh
+++ .photo-app/init.sh
@@ -9,28 +9,28 @@ fi
 # List of packages to check and install
 packages=("libwebp-dev" "fbi" "python3" "python3-pip")
 
-# # Update package list
-# echo "Updating package list..."
-# apt-get update
+# Update package list
+echo "Updating package list..."
+apt-get update
 
-# # Loop through the packages and install if not already installed
-# for pkg in "${packages[@]}"; do
-#   if ! dpkg -l | grep -qw $pkg; then
-#     echo "$pkg not found. Installing $pkg..."
-#     apt-get install -y $pkg
-#   else
-#     echo "$pkg is already installed."
-#   fi
-# done
+# Loop through the packages and install if not already installed
+for pkg in "${packages[@]}"; do
+  if ! dpkg -l | grep -qw $pkg; then
+    echo "$pkg not found. Installing $pkg..."
+    apt-get install -y $pkg
+  else
+    echo "$pkg is already installed."
+  fi
+done
 
-# # Set up the Python virtual environment
-# echo "Setting up Python virtual environment..."
-# python3 -m venv .venv
+# Set up the Python virtual environment
+echo "Setting up Python virtual environment..."
+python3 -m venv .venv
 source .venv/bin/activate
 
-# # Install Python packages from requirements.txt
-# echo "Installing required Python packages..."
-# pip install -r requirements.txt
+# Install Python packages from requirements.txt
+echo "Installing required Python packages..."
+pip install -r requirements.txt
 
 # Run the Python script
 echo "Running the Python script..."
