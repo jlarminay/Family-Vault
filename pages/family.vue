00060bf58d76bc4544c58573ac8c53814c19be7e
Josh <josh@ursacreative.ca>
2024-12-21 17:41:31 -0800

    Make image larger

--- pages/family.vue
+++ pages/family.vue
@@ -20,21 +20,10 @@ onMounted(() => {
   }
 });
 
-function zoomIn() {
-  if (panzoomInstance.value) {
-    panzoomInstance.value.zoomBy(0.2);
-  }
-}
-
-function zoomOut() {
-  if (panzoomInstance.value) {
-    panzoomInstance.value.zoomBy(-0.2);
-  }
-}
-
 function recenter() {
   if (panzoomInstance.value) {
-    panzoomInstance.value.reset();
+    panzoomInstance.value.zoomAbs(0, 0, 1);
+    panzoomInstance.value.moveTo(0, 0);
   }
 }
 </script>
@@ -45,43 +34,35 @@ function recenter() {
   </Head>
 
   <NuxtLayout name="app">
-    <main class="tw_p-1 sm:tw_px-6 sm:tw_py-4 tw_max-w-[1000px] tw_mx-auto">
-      <h1 class="h1">Family Tree</h1>
+    <main>
+      <div class="tw_p-1 sm:tw_px-6 sm:tw_py-4 tw_max-w-[1000px] tw_mx-auto">
+        <h1 class="h1">Family Tree</h1>
 
-      <div
-        class="tw_border-2 tw_rounded tw_border-orange-200 tw_bg-orange-50 tw_px-2 tw_py-1 tw_mt-4"
-      >
-        <p class="tw_text-lg">
-          This is a work-in-progress and may contain errors or inaccuracies. The content is intended
-          for entertainment purposes only and should not be relied upon for accuracy or
-          completeness.
-        </p>
-      </div>
-
-      <div class="tw_mt-4 tw_overflow-hidden tw_bg-[#ededed] tw_rounded-md tw_relative">
-        <div class="tw_p-2 tw_flex tw_gap-2 tw_absolute tw_z-10 tw_bottom-0 tw_right-0">
-          <q-btn
-            icon="o_zoom_in"
-            @click="zoomIn"
-            class="tw_aspect-square tw_bg-white tw_text-dark tw_p-0 tw_rounded-md"
-          />
-          <q-btn
-            icon="o_zoom_out"
-            @click="zoomOut"
-            class="tw_aspect-square tw_bg-white tw_text-dark tw_p-0 tw_rounded-md"
-          />
-          <q-btn
-            icon="o_crop_free"
-            @click="recenter"
-            class="tw_aspect-square tw_bg-white tw_text-dark tw_p-0 tw_rounded-md"
-          />
-        </div>
-
-        <div id="svg">
-          <img src="/assets/family-tree.svg" alt="Family Tree" />
+        <div
+          class="tw_border-2 tw_rounded tw_border-orange-200 tw_bg-orange-50 tw_px-2 tw_py-1 tw_mt-4"
+        >
+          <p class="tw_text-lg">
+            This is a work-in-progress and may contain errors or inaccuracies. The content is
+            intended for entertainment purposes only and should not be relied upon for accuracy or
+            completeness.
+          </p>
         </div>
       </div>
     </main>
+
+    <div class="tw_mt-4 tw_overflow-hidden tw_bg-[#ededed] tw_rounded-md tw_relative tw_m-4">
+      <div class="tw_p-2 tw_flex tw_gap-2 tw_absolute tw_z-10 tw_bottom-0 tw_right-0">
+        <q-btn
+          icon="o_crop_free"
+          @click="recenter"
+          class="tw_aspect-square tw_bg-white tw_text-dark tw_p-0 tw_rounded-md"
+        />
+      </div>
+
+      <div id="svg">
+        <img src="/assets/family-tree.svg" alt="Family Tree" />
+      </div>
+    </div>
   </NuxtLayout>
 </template>
 
